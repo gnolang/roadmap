2831b518625f39d88b55cbb6e7cb6240eb0b557a
Manfred Touron <94029+moul@users.noreply.github.com>
2023-04-11 21:39:16 +0200

    chore: fixup
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- gen-graph/main.go
+++ gen-graph/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"time"
+)
+
+type task struct {
+	ID                string    `json:"id,omitempty"`
+	CreatedAt         time.Time `json:"created_at,omitempty"`
+	UpdatedAt         time.Time `json:"updated_at,omitempty"`
+	LocalID           string    `json:"local_id,omitempty"`
+	Kind              int       `json:"kind,omitempty"`
+	Title             string    `json:"title,omitempty"`
+	Description       string    `json:"description,omitempty"`
+	Driver            int       `json:"driver,omitempty"`
+	State             int       `json:"state,omitempty"`
+	EstimatedDuration string    `json:"estimated_duration,omitempty"`
+	HasAuthor         string    `json:"has_author,omitempty"`
+	HasOwner          string    `json:"has_owner,omitempty"`
+	HasAssignee       []string  `json:"has_assignee,omitempty"`
+	HasLabel          []string  `json:"has_label,omitempty"`
+	IsDependingOn     []string  `json:"is_depending_on,omitempty"`
+	IsBlocking        []string  `json:"is_blocking,omitempty"`
+	IsRelatedWith     []string  `json:"is_related_with,omitempty"`
+}
 
 func main() {
-	fmt.Println("TEST")
+	file, err := ioutil.ReadFile("roadmap.json")
+	checkErr(err)
+	var tasks []task
+	err = json.Unmarshal(file, &tasks)
+	checkErr(err)
+
+	roadmap := make(map[string]task)
+	for _, t := range tasks {
+		roadmap[t.ID] = t
+	}
+
+	fmt.Println(roadmap)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
