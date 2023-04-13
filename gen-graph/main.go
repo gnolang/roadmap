41549981c9cb429dd0f5d6ee2bb35941281abb79
Manfred Touron <94029+moul@users.noreply.github.com>
2023-04-13 19:41:40 +0200

    chore: switch to new depviz (#25)

--- gen-graph/main.go
+++ gen-graph/main.go
@@ -1,50 +1,33 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"strings"
 	"time"
 
 	"github.com/goccy/go-graphviz"
 	"github.com/goccy/go-graphviz/cgraph"
+	"github.com/gogo/protobuf/jsonpb"
+	"moul.io/depviz/v3/pkg/dvmodel"
 	"moul.io/godev"
 )
-
-type task struct {
-	ID                string    `json:"id,omitempty"`
-	CreatedAt         time.Time `json:"created_at,omitempty"`
-	UpdatedAt         time.Time `json:"updated_at,omitempty"`
-	LocalID           string    `json:"local_id,omitempty"`
-	Kind              int       `json:"kind,omitempty"`
-	Title             string    `json:"title,omitempty"`
-	Description       string    `json:"description,omitempty"`
-	Driver            int       `json:"driver,omitempty"`
-	State             int       `json:"state,omitempty"`
-	EstimatedDuration string    `json:"estimated_duration,omitempty"`
-	HasAuthor         string    `json:"has_author,omitempty"`
-	HasOwner          string    `json:"has_owner,omitempty"`
-	HasAssignee       []string  `json:"has_assignee,omitempty"`
-	HasLabel          []string  `json:"has_label,omitempty"`
-	IsDependingOn     []string  `json:"is_depending_on,omitempty"`
-	IsBlocking        []string  `json:"is_blocking,omitempty"`
-	IsRelatedWith     []string  `json:"is_related_with,omitempty"`
-}
 
 func main() {
 	file, err := ioutil.ReadFile("output/roadmap.json")
 	checkErr(err)
-	var tasks []task
-	err = json.Unmarshal(file, &tasks)
+	var roadmapFile dvmodel.Batch
+	var u jsonpb.Unmarshaler
+	err = u.Unmarshal(bytes.NewReader(file), &roadmapFile)
 	checkErr(err)
 
-	roadmap := make(map[string]task)
-	for _, t := range tasks {
-		if t.Kind != 1 {
-			continue
+	roadmap := make(map[string]*dvmodel.Task)
+	for _, t := range roadmapFile.Tasks {
+		switch {
+		case t.Kind == dvmodel.Task_Issue:
+			roadmap[t.ID.String()] = t
 		}
-		roadmap[t.ID] = t
 	}
 
 	g := graphviz.New()
@@ -60,45 +43,44 @@ func main() {
 	nodes := make(map[string]*cgraph.Node)
 
 	for _, task := range roadmap {
-		node, err := graph.CreateNode(task.ID)
+		node, err := graph.CreateNode(task.ID.String())
 		checkErr(err)
 		if task.ID == "https://github.com/gnolang/roadmap/issues/1" {
-			println(godev.PrettyJSON(task))
+			println(godev.PrettyJSONPB(task))
 		}
 
 		// default
-		// node.SetLabel(fmt.Sprintf("#%s - %s", task.ShortID(), task.Title))
 		node.SetLabel(task.Title)
-		node.SetHref(task.ID)
+		node.SetHref(task.ID.String())
 		node.SetShape("box")
 		node.SetStyle("rounded")
 
 		// exceptions
-		if task.LabelExists("focus") {
+		if taskLabelExists(task, "focus") {
 			node.SetStyle("filled,bold,rounded")
 			node.SetFillColor("#ffeeee")
 		}
-		if task.LabelExists("vision") {
+		if taskLabelExists(task, "vision") {
 			node.SetStyle("filled,rounded")
 			node.SetFillColor("#eeeeee")
 		}
 
-		nodes[task.ID] = node
+		nodes[task.ID.String()] = node
 	}
 
 	for _, task := range roadmap {
 		for _, dependentID := range task.IsBlocking {
-			dependent := roadmap[dependentID]
-			name := task.ID + dependent.ID
-			edge, err := graph.CreateEdge(name, nodes[task.ID], nodes[dependent.ID])
+			dependent := roadmap[dependentID.String()]
+			name := task.ID.String() + dependent.ID.String()
+			edge, err := graph.CreateEdge(name, nodes[task.ID.String()], nodes[dependent.ID.String()])
 			checkErr(err)
 			_ = edge
 			// edge.SetLabel("blocking")
 		}
 		for _, dependingID := range task.IsDependingOn {
-			depending := roadmap[dependingID]
-			name := depending.ID + task.ID
-			edge, err := graph.CreateEdge(name, nodes[depending.ID], nodes[task.ID])
+			depending := roadmap[dependingID.String()]
+			name := depending.ID.String() + task.ID.String()
+			edge, err := graph.CreateEdge(name, nodes[depending.ID.String()], nodes[task.ID.String()])
 			checkErr(err)
 			_ = edge
 			// edge.SetLabel("depending")
@@ -108,18 +90,14 @@ func main() {
 	checkErr(g.RenderFilename(graph, graphviz.XDOT, "output/roadmap.dot"))
 }
 
-func (t task) LabelExists(label string) bool {
+func taskLabelExists(t *dvmodel.Task, label string) bool {
 	for _, l := range t.HasLabel {
-		short := l[strings.LastIndex(l, "/")+1:]
+		short := l.String()[strings.LastIndex(l.String(), "/")+1:]
 		if short == label {
 			return true
 		}
 	}
 	return false
-}
-
-func (t task) ShortID() string {
-	return t.ID[strings.LastIndex(t.ID, "/")+1:]
 }
 
 func checkErr(err error) {
